d81e5707bd5a76945d7f0629a5efb02ad7f9621a
Santiago Bernal <exactlimon@gmail.com>
2022-09-05 16:23:55 -0500

    [add] pwa config files

---
+++ src/serviceWorker.js
@@ -0,0 +1,28 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable arrow-parens */
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
+const universitaria = 'universitaria-stereo-v1';
+const assets = [
+  '/',
+  'index.html',
+  'img/favicon.png',
+  'img/emisora_universitaria_estereo.png'
+]
+
+// eslint-disable-next-line arrow-parens
+self.addEventListener('install', installEvent => {
+  installEvent.waitUntil(
+    caches.open(universitaria).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    }),
+  );
+});
